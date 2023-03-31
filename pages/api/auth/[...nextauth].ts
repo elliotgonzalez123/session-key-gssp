790fe7ad2947983e1e1ea04d2e9d4944fbc43f08
Elliot Gonzalez <elliotgrey@gmail.com>
2023-03-31 09:09:46 -0400

    init commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+};
+
+export default NextAuth(authOptions);
